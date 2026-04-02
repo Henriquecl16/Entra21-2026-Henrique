365bebcc4fcaff7e22209f036ea0c02915be6f3a
Guest Blumenau <guest.bu@alunos.sc.senac.br>
2026-04-02 15:57:11 -0300

    Resolvi o exercicio 11

--- src/AulaDesviocondicional.java
+++ src/AulaDesviocondicional.java
@@ -12,7 +12,8 @@ public class AulaDesviocondicional {
 		//exercicio7();
 		//exercicio8();
 		//exercicio9();
-		exercicio10();
+		//exercicio10();
+		exercicio11();
 		
 	}
 	static void verificarNumeroPar() {
@@ -229,6 +230,26 @@ public class AulaDesviocondicional {
         scanner.close();
 		
 	}
+	
+	static void exercicio11() {
+		
+		 Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Digite um ano: ");
+	        int ano = scanner.nextInt();
+
+	       
+	        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+	            System.out.println(ano + " é um ano bissexto.");
+	        } else {
+	            System.out.println(ano + " NÃO é um ano bissexto.");
+	        }
+
+	        scanner.close();
+		
+	}
+	
+	
 	
 	
 	
