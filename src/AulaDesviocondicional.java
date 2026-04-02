b67e816d441e05e1f586599106663648cfa19123
Guest Blumenau <guest.bu@alunos.sc.senac.br>
2026-04-02 15:50:20 -0300

    Resolvi o exercicio 9

--- src/AulaDesviocondicional.java
+++ src/AulaDesviocondicional.java
@@ -10,7 +10,8 @@ public class AulaDesviocondicional {
 		//exercicio5();
 		//exercicio6();
 		//exercicio7();
-		exercicio8();
+		//exercicio8();
+		exercicio9();
 		
 	}
 	static void verificarNumeroPar() {
@@ -167,6 +168,40 @@ public class AulaDesviocondicional {
         }
 
         scanner.close();		
+		
+	}
+	
+	static void exercicio9() {
+		
+	      Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Digite o lado A: ");
+	        double a = scanner.nextDouble();
+
+	        System.out.print("Digite o lado B: ");
+	        double b = scanner.nextDouble();
+
+	        System.out.print("Digite o lado C: ");
+	        double c = scanner.nextDouble();
+
+	       
+	        if (a + b > c && a + c > b && b + c > a) {
+	            System.out.println("Os lados formam um triângulo válido!");
+
+	           
+	            if (a == b && b == c) {
+	                System.out.println("Tipo: Equilátero");
+	            } else if (a == b || a == c || b == c) {
+	                System.out.println("Tipo: Isósceles");
+	            } else {
+	                System.out.println("Tipo: Escaleno");
+	            }
+
+	        } else {
+	            System.out.println("Os lados NÃO formam um triângulo.");
+	        }
+
+	        scanner.close();
 		
 	}
 	
