a1cb22c7abd853d7257564c37668156abf4687e2
Guest Blumenau <guest.bu@alunos.sc.senac.br>
2026-04-02 14:51:29 -0300

    Criado Solução do exercico numero par

--- src/AulaDesviocondicional.java
+++ src/AulaDesviocondicional.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
 public class AulaDesviocondicional {
-
+	
+	public static void main(String[] args) {
+		verificarNumeroPar();
+	}
+	static void verificarNumeroPar() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Informe numero:");
+		int numero = sc.nextInt();
+		
+		if(numero % 2 == 0) {
+			System.out.println("Numero par");
+		}
+		
+		
+		
+	}
 }
